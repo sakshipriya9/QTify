cb995e1e9b5303a8102a8a13da3ab5e2a8501f36
Sakshi Priya <priyasakshi8989@gmail.com>
2024-04-14 19:05:22 +0530

    ME_QTIFY_MODULE_NAVBAR

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import NavBar from './components/NavBar/NavBar';
+import './components/NavBar/NavBar.css';
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+      {/* Other components or content */}
+    </div>
+  );
+}
+
+export default App;
